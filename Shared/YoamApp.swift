d35ff0703c18605e3942e050ceae4039e529f0e7
Corey Sutton <Corey.sutton7@gmail.com>
2021-06-19 22:03:44 -0400

    Initial Commit

---
+++ Shared/YoamApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoamApp.swift
+//  Shared
+//
+//  Created by user198524 on 6/19/21.
+//
+
+import SwiftUI
+
+@main
+struct YoamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
